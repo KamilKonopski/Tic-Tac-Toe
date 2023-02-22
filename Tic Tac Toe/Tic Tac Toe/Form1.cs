96e34eb3874f156ca13748e2f959369d3a98795b
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-02-22 21:22:30 +0100

    add little styling

--- Tic Tac Toe/Tic Tac Toe/Form1.cs
+++ Tic Tac Toe/Tic Tac Toe/Form1.cs
@@ -33,92 +33,101 @@ namespace Tic_Tac_Toe
             // 
             // btn1
             // 
+            this.btn1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btn1.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btn1.Location = new System.Drawing.Point(35, 52);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(150, 150);
             this.btn1.TabIndex = 0;
-            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.UseVisualStyleBackColor = false;
             this.btn1.Click += new System.EventHandler(this.btn1_Click);
             // 
             // btn2
             // 
+            this.btn2.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btn2.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btn2.Location = new System.Drawing.Point(191, 52);
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(150, 150);
             this.btn2.TabIndex = 1;
-            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.UseVisualStyleBackColor = false;
             this.btn2.Click += new System.EventHandler(this.btn2_Click);
             // 
             // btn3
             // 
+            this.btn3.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btn3.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btn3.Location = new System.Drawing.Point(347, 52);
             this.btn3.Name = "btn3";
             this.btn3.Size = new System.Drawing.Size(150, 150);
             this.btn3.TabIndex = 2;
-            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.UseVisualStyleBackColor = false;
             this.btn3.Click += new System.EventHandler(this.btn3_Click);
             // 
             // btn4
             // 
+            this.btn4.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btn4.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btn4.Location = new System.Drawing.Point(35, 208);
             this.btn4.Name = "btn4";
             this.btn4.Size = new System.Drawing.Size(150, 150);
             this.btn4.TabIndex = 3;
-            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.UseVisualStyleBackColor = false;
             this.btn4.Click += new System.EventHandler(this.btn4_Click);
             // 
             // btn5
             // 
+            this.btn5.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btn5.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btn5.Location = new System.Drawing.Point(191, 208);
             this.btn5.Name = "btn5";
             this.btn5.Size = new System.Drawing.Size(150, 150);
             this.btn5.TabIndex = 4;
-            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.UseVisualStyleBackColor = false;
             this.btn5.Click += new System.EventHandler(this.btn5_Click);
             // 
             // btn6
             // 
+            this.btn6.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btn6.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btn6.Location = new System.Drawing.Point(347, 208);
             this.btn6.Name = "btn6";
             this.btn6.Size = new System.Drawing.Size(150, 150);
             this.btn6.TabIndex = 5;
-            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.UseVisualStyleBackColor = false;
             this.btn6.Click += new System.EventHandler(this.btn6_Click);
             // 
             // btn7
             // 
+            this.btn7.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btn7.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btn7.Location = new System.Drawing.Point(35, 364);
             this.btn7.Name = "btn7";
             this.btn7.Size = new System.Drawing.Size(150, 150);
             this.btn7.TabIndex = 6;
-            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.UseVisualStyleBackColor = false;
             this.btn7.Click += new System.EventHandler(this.btn7_Click);
             // 
             // btn8
             // 
+            this.btn8.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btn8.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btn8.Location = new System.Drawing.Point(191, 364);
             this.btn8.Name = "btn8";
             this.btn8.Size = new System.Drawing.Size(150, 150);
             this.btn8.TabIndex = 7;
-            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.UseVisualStyleBackColor = false;
             this.btn8.Click += new System.EventHandler(this.btn8_Click);
             // 
             // btn9
             // 
+            this.btn9.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btn9.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btn9.Location = new System.Drawing.Point(347, 364);
             this.btn9.Name = "btn9";
             this.btn9.Size = new System.Drawing.Size(150, 150);
             this.btn9.TabIndex = 8;
-            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.UseVisualStyleBackColor = false;
             this.btn9.Click += new System.EventHandler(this.btn9_Click);
             // 
             // label1
@@ -205,6 +214,7 @@ namespace Tic_Tac_Toe
             // 
             // Form1
             // 
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
             this.ClientSize = new System.Drawing.Size(884, 611);
             this.Controls.Add(this.btnNewGame);
             this.Controls.Add(this.resultX);
@@ -259,6 +269,7 @@ namespace Tic_Tac_Toe
             {
                 button.Enabled = true;
                 button.Text = "";
+                button.BackColor = System.Drawing.Color.Empty;
             }
         }
 
@@ -266,34 +277,58 @@ namespace Tic_Tac_Toe
         {
             if (btn1.Text != "" && btn1.Text == btn2.Text && btn2.Text == btn3.Text)
             {
+                btn1.BackColor = System.Drawing.Color.Green;
+                btn2.BackColor = System.Drawing.Color.Green;
+                btn3.BackColor = System.Drawing.Color.Green;
                 Won();
             }
             else if (btn4.Text != "" && btn4.Text == btn5.Text && btn5.Text == btn6.Text)
             {
+                btn4.BackColor = System.Drawing.Color.Green;
+                btn5.BackColor = System.Drawing.Color.Green;
+                btn6.BackColor = System.Drawing.Color.Green;
                 Won();
             }
             else if (btn7.Text != "" && btn7.Text == btn8.Text && btn8.Text == btn9.Text)
             {
+                btn7.BackColor = System.Drawing.Color.Green;
+                btn8.BackColor = System.Drawing.Color.Green;
+                btn9.BackColor = System.Drawing.Color.Green;
                 Won();
             }
             else if (btn1.Text != "" && btn1.Text == btn4.Text && btn4.Text == btn7.Text)
             {
+                btn1.BackColor = System.Drawing.Color.Green;
+                btn4.BackColor = System.Drawing.Color.Green;
+                btn7.BackColor = System.Drawing.Color.Green;
                 Won();
             }
             else if (btn2.Text != "" && btn2.Text == btn5.Text && btn5.Text == btn8.Text)
             {
+                btn2.BackColor = System.Drawing.Color.Green;
+                btn5.BackColor = System.Drawing.Color.Green;
+                btn8.BackColor = System.Drawing.Color.Green;
                 Won();
             }
             else if (btn3.Text != "" && btn3.Text == btn6.Text && btn6.Text == btn9.Text)
             {
+                btn3.BackColor = System.Drawing.Color.Green;
+                btn6.BackColor = System.Drawing.Color.Green;
+                btn9.BackColor = System.Drawing.Color.Green;
                 Won();
             }
             else if (btn1.Text != "" && btn1.Text == btn5.Text && btn5.Text == btn9.Text)
             {
+                btn1.BackColor = System.Drawing.Color.Green;
+                btn5.BackColor = System.Drawing.Color.Green;
+                btn9.BackColor = System.Drawing.Color.Green;
                 Won();
             }
             else if (btn3.Text != "" && btn3.Text == btn5.Text && btn5.Text == btn7.Text)
             {
+                btn3.BackColor = System.Drawing.Color.Green;
+                btn5.BackColor = System.Drawing.Color.Green;
+                btn7.BackColor = System.Drawing.Color.Green;
                 Won();
             }
             else if (move == 9)
